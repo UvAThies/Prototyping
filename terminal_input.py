280665c164d475a2919fe6e33706cb8ceeee551a
Thies <git@thies.dev>
2024-03-12 11:33:19 +0100

    Added some code to control the robot

---
+++ terminal_input.py
@@ -0,0 +1,28 @@
+import controls
+import time
+def main():
+
+    controls.stop()
+
+    # listen for arrow keys
+    while True:
+        key = input()
+        trigger_input = 0
+        joy_input = 127
+
+        if "w" in key:
+            trigger_input = 1
+        elif "s" in key:
+            trigger_input = -1
+        if "a" in key:
+            joy_input = 0
+        elif "d" in key:
+            joy_input = 255
+
+        controls.motor_instructions(trigger_input, joy_input)
+        time.sleep(1)
+        controls.stop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ terminal_live_input.py
@@ -0,0 +1,30 @@
+import controls
+import time
+import keyboard
+
+def main():
+
+    controls.stop()
+
+    # listen for arrow keys
+    while True:  # making a loop
+        trigger_input = 0
+        joy_input = 127
+        print(keyboard)
+        if keyboard.is_pressed('w'):
+            trigger_input = 1
+        elif keyboard.is_pressed('s'):
+            trigger_input = -1
+
+        if keyboard.is_pressed('a'):
+            joy_input = 0
+        elif keyboard.is_pressed('d'):
+            joy_input = 255
+
+        controls.motor_instructions(trigger_input, joy_input)
+
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
