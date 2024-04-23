da7e956764add8362cfb7e9037edc938e2dc5f6f
Thies <git@thies.dev>
2024-04-23 11:46:18 +0200

    First GPS

--- webcontrol.py
+++ webcontrol.py
@@ -5,13 +5,17 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 from controls import MotorControl, ServoControl
+import gpsd
+
 
 # from controller_sim import MotorControlSim
 # controller = MotorControlSim()
 
+gpsd.connect()
 controller = MotorControl()
 servo_controller = ServoControl()
 controller.setup_sig_handler()
+
 
 # intialize the Flask app
 app = Flask(__name__)
@@ -60,6 +64,33 @@ def handle_connect():
 @socketio.on("disconnect")
 def handle_disconnect():
     print("Client disconnected")
+
+
+# Use this function to get the current GPS data
+def get_gps_data():
+    current = gpsd.get_current()
+
+    # Check if nofix
+    if current.mode < 2:
+        return {"lat": 0, "lon": 0, "speed": 0, "heading": 0,
+                "mode": current.mode,
+        "status": "NOFIX"}
+
+    return {
+        "lat": current.lat,
+        "lon": current.lon,
+        "speed": current.hspeed,
+        "heading": current.track,
+        "mode": current.mode,
+        "status": "OK",
+    }
+
+
+@socketio.on("gps")
+def handle_gps(data):
+    gps_data = get_gps_data()
+    print(gps_data)
+    return gps_data
 
 
 if __name__ == "__main__":
