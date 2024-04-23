fe0bc781125911374c3a71fa8cd9c1b7202a539d
Thies <git@thies.dev>
2024-04-23 12:36:23 +0200

    GPS!

--- webcontrol.py
+++ webcontrol.py
@@ -8,9 +8,6 @@ from controls import MotorControl, ServoControl
 import gpsd
 
 
-# from controller_sim import MotorControlSim
-# controller = MotorControlSim()
-
 gpsd.connect()
 controller = MotorControl()
 servo_controller = ServoControl()
@@ -69,12 +66,24 @@ def handle_disconnect():
 # Use this function to get the current GPS data
 def get_gps_data():
     current = gpsd.get_current()
+    # return {
+    #     "lat": 52.3553194,
+    #     "lon": 4.9571569,
+    #     "hspeed": 0,
+    #     "track": 0,
+    #     "mode": 3,
+    #     "time": "2024-01-01T00:00:00.000Z",
+    #     "error": {
+    #         "x": 0.1,
+    #         "y": 0.1,
+    #         "z": 0.1,
+    #     }
+    # }
 
     # Check if nofix
     if current.mode < 2:
         return {"lat": 0, "lon": 0, "speed": 0, "heading": 0,
-                "mode": current.mode,
-        "status": "NOFIX"}
+                "mode": current.mode, time: current.time}
 
     return {
         "lat": current.lat,
@@ -82,7 +91,8 @@ def get_gps_data():
         "speed": current.hspeed,
         "heading": current.track,
         "mode": current.mode,
-        "status": "OK",
+        "time": current.time,
+        "error": current.error
     }
 
 
